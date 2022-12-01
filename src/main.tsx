af6f30e8878760d50224d2a17a9218c1dbf9cdd7
Benjamin Charles <benjaminpcharles@gmail.com>
2022-12-01 11:46:23 +0100

    first implementation redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './global';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Fragment>
-    <GlobalStyle />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <Provider store={store}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      </Provider>
     </Fragment>
   </React.StrictMode>
 );
